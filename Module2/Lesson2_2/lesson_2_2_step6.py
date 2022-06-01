d7113cc289c8be9e97143847e792ed647b307cbb
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2022-06-01 22:45:16 +0300

    add lesson_2_2_step6 solution

---
+++ Module2/Lesson2_2/lesson_2_2_step6.py
@@ -0,0 +1,9 @@
+import Module2.Lesson2_1.lesson_2_1_step5 as main_step
+
+
+if __name__ == "__main__":
+    page = "http://SunInJuly.github.io/execute_script.html"
+    # пройди квест ;-)
+    # решение выполнено в шаге 5 урока 1 модуля 2 (lesson_2_1_step5), кроме функции get_x
+    get_x = main_step.get_x
+    main_step.complete_form(page, get_x)
