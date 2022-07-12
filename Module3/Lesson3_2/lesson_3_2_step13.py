b630bb86967b77680ba9e68a1e881a2599331e55
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2022-07-12 19:57:18 +0300

    add solution for lesson_3_2_step13

--- Module3/Lesson3_2/lesson_3_2_step13.py
+++ Module3/Lesson3_2/lesson_3_2_step13.py
@@ -1,8 +1,38 @@
+import time
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class TestForm(unittest.TestCase):
     def test_registration1(self):
-        pass
+        link = "http://suninjuly.github.io/registration1.html"
+        self.assertEqual(fill_form(link), "Congratulations! You have successfully registered!")
 
     def test_registration2(self):
-        pass
+        link = "http://suninjuly.github.io/registration2.html"
+        self.assertEqual(fill_form(link), "Congratulations! You have successfully registered!")
+
+
+def fill_form(page):
+    # Data for required fields
+    first_name = "FirstName"
+    last_name = "LastName"
+    email = "E-Mail@example.com"
+    browser = webdriver.Chrome()
+
+    browser.get(page)
+    # Fill required fields
+    browser.find_element(By.CSS_SELECTOR, "input[required].first").send_keys(first_name)
+    browser.find_element(By.CSS_SELECTOR, "input[required].second").send_keys(last_name)
+    browser.find_element(By.CSS_SELECTOR, "input[required].third").send_keys(email)
+
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+    time.sleep(2)
+
+    # return element for checking registration
+    return browser.find_element(By.TAG_NAME, "h1").text
+
+
+if __name__ == "__main__":
+    unittest.main()
