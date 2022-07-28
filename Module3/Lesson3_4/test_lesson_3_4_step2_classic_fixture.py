1b2d24c02c48ce23288269849edb4cac0d8268b3
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2022-07-28 19:56:15 +0300

    add tests to lesson_3_4_step2_classic_fixture.py
    - add TestMainPage2 class with method fixtures

--- Module3/Lesson3_4/test_lesson_3_4_step2_classic_fixture.py
+++ Module3/Lesson3_4/test_lesson_3_4_step2_classic_fixture.py
@@ -23,3 +23,21 @@ class TestMainPage1:
     def test_guest_should_see_basket_link_on_the_main_page(self):
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
+class TestMainPage2:
+    def setup_method(self):
+        print("start browser for test..")
+        self.browser = webdriver.Chrome()
+
+    def teardown_method(self):
+        print("quit browser for test..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
